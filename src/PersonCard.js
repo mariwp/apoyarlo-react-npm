c234c9462c2d2aafd92c9844897bd62d98bfafd1
mariwp <142070393+mariwp@users.noreply.github.com>
2024-07-08 00:58:47 -0400

    Added birthday button to increase age in PersonCard

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
-    const { firstName, lastName, age, hairColor } = props;
+    const { firstName, lastName, hairColor } = props;
+    const [age, setAge] = useState(props.age);
+
+    const handleBirthday = () => {
+        setAge(age + 1);
+    };
+
     return (
         <div>
             <h2>{lastName}, {firstName}</h2>
             <p>Age: {age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={handleBirthday}>Birthday Button for {firstName} {lastName}</button>
         </div>
     );
 };
